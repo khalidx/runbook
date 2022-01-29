9686e71739efa581e2af149548330ab6126e5186
Khalid Zoabi <kzoabi@outlook.com>
2022-01-29 21:54:44 +0000

    Removing dev CLI flag since it conflicts with args parsing and completions

--- bin/cli.js
+++ bin/cli.js
@@ -1,9 +1,6 @@
 #!/usr/bin/env node
 
-const dev = process.argv.indexOf('--dev')
-
-if (dev > -1 || process.env.DEV !== undefined) {
-  process.argv.splice(dev, 1)
+if (process.env.DEV !== undefined) {
   require('ts-node/register')
   require('../src/cli.ts')
 } else {
